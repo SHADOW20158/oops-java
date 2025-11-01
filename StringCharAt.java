a7db898504b52789d5d388879307280c033c6e3c
Arpit Pandey <ansh2011pandey@gmail.com>
2025-11-01 15:02:26 +0530

    Add CharAtExample class to demonstrate charAt method

---
+++ StringCharAt.java
@@ -0,0 +1,12 @@
+public class CharAtExample {
+    public static void main(String[] args) {
+        // Define the string
+        String s = "Java";
+
+        // Get the character at index 2 (third character)
+        char result = s.charAt(2);
+
+        // Print the result
+        System.out.println("The character at index 2 is: " + result);
+    }
+}
